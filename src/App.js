abc723711b2cce35ff95b7924cabc49e1b7df631
David Tang <dtang@auditboard.com>
2020-09-14 11:01:59 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,93 +1,77 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { fetchPosts, fetchInfo, parseSubredditId } from "./api/RedditClient";
+import SubredditPosts from "./components/SubredditPosts";
+import SubredditInfo from "./components/SubredditInfo";
+import Loader from "./components/Loader";
+import "./App.css";
 
-import { fetchPosts, fetchInfo, parseSubredditId } from './api/RedditClient';
-import SubredditPosts from './components/SubredditPosts.jsx';
-import SubredditInfo from './components/SubredditInfo.jsx';
-import Loader from './components/Loader.jsx';
-
-function App() {
+export default function App() {
   // Holds the data from api responses
-  const [data, setData] = React.useState({
+  const [data, setData] = useState({
     posts: null,
-    info: null
+    info: null,
   });
 
-  // Whether or not error message should be displayed
-  const [showError, setShowError] = React.useState(false);
+  const [loading, setLoading] = useState(false);
 
-  // Whether or not page is loading
-  const [loading, setLoading] = React.useState(false);
-
-  // Last user submission
-  const [input, setInput] = React.useState('');
-
-  // Bind the `handleSubmit` function to changes in the
-  // input state
-  React.useEffect(handleSubmit, [input]);
-
-  /**
-   * Handles api calls/side effects from user submitting
-   * form
-   */
-  function handleSubmit() {
-    if (showError) setShowError(false);
-    if (input === '') return;
+  function search(input) {
     setLoading(true);
     setData({
       posts: null,
-      info: null
+      info: null,
     });
-    let posts;
+
     fetchPosts(input)
-      .then(res => {
-        posts = res;
-        return parseSubredditId(res);
+      .then((posts) => {
+        const subredditId = parseSubredditId(posts);
+
+        return Promise.all([posts, fetchInfo(subredditId)]);
       })
-      .then(id => fetchInfo(id))
-      .then(info => {
+      .then((data) => {
+        const [posts, info] = data;
+
         setData({
           posts,
-          info
-        })
+          info,
+        });
+
         setLoading(false);
-      })
-      .catch(err => {
-        console.log(err);
-        setLoading(false);
-        setShowError(true);
       });
   }
 
-  /**
-   * Submit callback for subreddit. Returns function to
-   * set input state based on given subreddit from button
-   * triggered event
-   *
-   * @param subreddit - the subreddit to look up
-   * @return function with event object as param
-   */
-  function onSubmit(subreddit) {
-    return function(e) {
-      e.preventDefault();
-      setInput(subreddit);
-    }
+  function handleJavaScriptSearch(e) {
+    e.preventDefault();
+    search("javascript");
+  }
+
+  function handleJavaSearch(e) {
+    e.preventDefault();
+    search("java");
+  }
+
+  function handleCatsSearch(e) {
+    e.preventDefault();
+    search("cats");
   }
 
   return (
     <div className="App">
       <form>
-        <button type="button" onClick={onSubmit("javascript")}>r/javascript</button>
-        <button type="button" onClick={onSubmit("java")}>r/java</button>
-        <button type="button" onClick={onSubmit("cats")}>r/cats</button>
+        <button type="button" onClick={handleJavaScriptSearch}>
+          r/javascript
+        </button>
+        <button type="button" onClick={handleJavaSearch}>
+          r/java
+        </button>
+        <button type="button" onClick={handleCatsSearch}>
+          r/cats
+        </button>
       </form>
-      {data.info && <SubredditInfo info={data.info}/>}
-      <br/><br/>
-      {data.posts && <SubredditPosts posts={data.posts}/>}
-      {showError && <p>Error loading that subreddit</p>}
-      <Loader active={loading}/>
+      {data.info && <SubredditInfo info={data.info} />}
+      <br />
+      <br />
+      {data.posts && <SubredditPosts posts={data.posts} />}
+      {loading && <Loader />}
     </div>
   );
 }
-
-export default App;
